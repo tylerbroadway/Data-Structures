67f04735fd5d2b7f2cf7245afb8e09eee5c6b2ad
tyler broadway <tbroadway96@gmail.com>
2020-08-10 12:43:33 -0500

    Created Linked List class

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,7 +1,30 @@
 class Node:
-    def __init(self, value = None, next_node = None):
+    def __init(self, value=None, next_node=None):
         self.value = value
-        self.next = next_node
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def set_next(self, new_next):
+        self.next_node = new_next
 
 class LinkedList:
-    pass
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_head(self, val):
+        pass
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
+    
+    def remove_from_tail(self):
+        pass
