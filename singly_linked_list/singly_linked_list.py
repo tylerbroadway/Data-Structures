1663a3ab4b74300c8a23a662219ae028e6246609
tyler broadway <tbroadway96@gmail.com>
2020-08-10 15:41:12 -0500

    Finished methods for LinkedList

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,10 +1,13 @@
 class Node:
-    def __init(self, value=None, next_node=None):
+    def __init__(self, value, next_node=None):
         self.value = value
         self.next_node = next_node
 
     def get_value(self):
         return self.value
+
+    def get_next(self):
+        return self.next_node
 
     def set_next(self, new_next):
         self.next_node = new_next
@@ -14,17 +17,42 @@ class LinkedList:
         self.head = None
         self.tail = None
 
-    def add_to_head(self, val):
-        pass
-
     def add_to_tail(self, value):
         new_node = Node(value)
-        if not self.head:
+        if self.head is None and self.tail is None:
             self.head = new_node
             self.tail = new_node
         else:
-            self.tail.next_node = new_node
+            self.tail.set_next(new_node)
             self.tail = new_node
     
-    def remove_from_tail(self):
-        pass
+    def remove_tail(self):
+        if self.head is None and self.tail is None:
+            return None
+
+        if self.head == self.tail:
+            removed_val = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return removed_val
+        else:
+            val = self.tail.get_value()
+            current = self.head
+            while current.get_next() != self.tail:
+                current = current.get_next()
+            self.tail = current
+            self.tail.set_next(None)
+            return val
+
+    def remove_head(self):
+        if self.head is None and self.tail is None:
+            return None
+        if self.head == self.tail:
+            val = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return val
+        else:
+            val = self.head.get_value()
+            self.head = self.head.get_next()
+            return val
